d0ffac7084113b3281117014d24bc6406727351d
Jennifer Carreno <jennifercarreno72@gmail.com>
2021-12-01 19:32:58 -0800

    created basic home, users can post habits

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
+# from bson.objectid import ObjectId
+from pymongo import MongoClient
+import os
+
+host = os.environ.get('DB_URL')
+client = MongoClient(host=host)
+db = client.habitLog
+habits = db.habits
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World'
+    return render_template('home.html', habits=habits.find())
+
+@app.route('/habits/new')
+def habits_new():
+    return render_template('habit_new.html')
+
+@app.route('/habits', methods=['POST'])
+def habits_submit():
+    habit = {
+        'name': request.form.get('name'),
+    }
+    habits.insert_one(habit)
+    print(habits)
+    return redirect(url_for('.index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
