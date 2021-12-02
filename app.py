e9a45f7cad94f825dcb74d749526fddc5de41b08
Jennifer Carreno <jennifercarreno72@gmail.com>
2021-12-01 19:41:14 -0800

    users can delete playlists

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
-# from bson.objectid import ObjectId
+from bson.objectid import ObjectId
 from pymongo import MongoClient
 import os
 
@@ -10,14 +10,17 @@ habits = db.habits
 
 app = Flask(__name__)
 
+# home page
 @app.route('/')
 def index():
     return render_template('home.html', habits=habits.find())
 
+# prompts to create a new habit
 @app.route('/habits/new')
 def habits_new():
     return render_template('habit_new.html')
 
+# displays new habit on log
 @app.route('/habits', methods=['POST'])
 def habits_submit():
     habit = {
@@ -26,6 +29,11 @@ def habits_submit():
     habits.insert_one(habit)
     print(habits)
     return redirect(url_for('.index'))
+
+@app.route('/habits/<habit_id>/delete', methods=['POST'])
+def habits_delete(habit_id):
+    habits.delete_one({'_id': ObjectId(habit_id)})
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
