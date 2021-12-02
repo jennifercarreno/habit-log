2d62c8f68377a50ece5cfe9b4fecc95a59412d9b
Jennifer Carreno <jennifercarreno72@gmail.com>
2021-12-01 19:59:29 -0800

    users can edit and  update habits

--- app.py
+++ app.py
@@ -30,9 +30,29 @@ def habits_submit():
     print(habits)
     return redirect(url_for('.index'))
 
+# deletes a habit
 @app.route('/habits/<habit_id>/delete', methods=['POST'])
 def habits_delete(habit_id):
     habits.delete_one({'_id': ObjectId(habit_id)})
+    return redirect(url_for('index'))
+
+# edit a habit
+@app.route('/habits/<habit_id>/edit')
+def habits_edit(habit_id):
+    habit = habits.find_one({'_id': ObjectId(habit_id)})
+    return render_template('habit_edit.html', habit=habit)
+
+# update a habit
+@app.route('/habits/<habit_id>', methods=['POST'])
+def habits_update(habit_id):
+    updated_habit = {
+        'name': request.form.get('name'),
+    }
+    # set the former playlist to the new one we just updated/edited
+    habits.update_one(
+        {'_id': ObjectId(habit_id)},
+        {'$set': updated_habit})
+    # take us back to the playlist's show page
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
